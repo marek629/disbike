ccaccf8486db5ed272053f380e140a12d9c526d8
Marek Jędryka <marek629@poczta.onet.pl>
2015-06-17 23:32:55 +0200

    CreateEvent was persisted.

--- src/DisBike/BikeBundle/Controller/DefaultController.php
+++ src/DisBike/BikeBundle/Controller/DefaultController.php
@@ -1,15 +1,18 @@
 <?php
-
 namespace DisBike\BikeBundle\Controller;
+
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use DisBike\BikeBundle\Entity\CreateEvent;
 use DisBike\BikeBundle\Form\CreateEventType;
 
+use DateTime;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+
 
 class DefaultController extends Controller
 {
@@ -20,15 +23,29 @@ class DefaultController extends Controller
     public function createAction(Request $request)
     {
         $bike = new CreateEvent();
-        
+
         $form = $this->createForm(new CreateEventType(), $bike);
-        
+
         $form->handleRequest($request);
-        
+
         if ($form->isValid()) {
             echo 'Create request is '.$request->getMethod();
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $createEvent = $form->getData();
+            $createEvent->setBikeId(
+                strtolower($createEvent->getBrandName()).'-'.
+                strtolower($createEvent->getModelName()).'-'.
+                number_format(microtime(true), 4, '', ''));
+            $createEvent->setCreatedDate(new DateTime());
+
+            $entityManager->persist($createEvent);
+            $entityManager->flush();
+
+            //return $this->redirectToRoute(...);
         }
-                    
+
         return array('form' => $form->createView());
     }
 }
