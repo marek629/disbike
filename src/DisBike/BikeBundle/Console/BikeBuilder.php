6806870938cc2cd564248c6a8a3c8371a76098bc
Marek Jędryka <marek629@poczta.onet.pl>
2015-07-21 20:24:24 +0200

    Clearing read model was added.

--- src/DisBike/BikeBundle/Console/BikeBuilder.php
+++ src/DisBike/BikeBundle/Console/BikeBuilder.php
@@ -4,10 +4,14 @@ namespace DisBike\BikeBundle\Console;
 
 use DisBike\BikeBundle\Entity\Bike;
 
+use Doctrine\ORM\EntityManager;
+
 
 class BikeBuilder {
-    public function buildReadModel($entityManager)
+    public function buildReadModel(EntityManager $entityManager)
     {
+        $this->clearBikeRepository($entityManager);
+
         $repository = $entityManager->getRepository('BikeBundle:CreateEvent');
 
         foreach ($repository->findAll() as $bikeCreateEvent) {
@@ -20,5 +24,10 @@ class BikeBuilder {
             $entityManager->persist($bike);
             $entityManager->flush();
         }
+    }
+
+    private function clearBikeRepository(EntityManager $entityManager)
+    {
+        $entityManager->createQuery('DELETE FROM BikeBundle:Bike')->execute();
     }
 }
